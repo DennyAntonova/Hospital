26f1d2827c35b3dba35a775158f324992d98eed0
DennyAntonova <83185824+DennyAntonova@users.noreply.github.com>
2021-11-06 01:03:18 +0200

    Update Doctors.java

--- src/com/company/Doctors.java
+++ src/com/company/Doctors.java
@@ -1,4 +1,57 @@
 package com.company;
 
 public class Doctors {
+    public String getDoctor_id() {
+        return doctor_id;
+    }
+
+    public void setDoctor_id(String doctor_id) {
+        this.doctor_id = doctor_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    private String doctor_id;
+    private String first_name;
+    private String last_name;
+    private String speciality;
+
+    public Doctors(String doctor_id, String first_name, String last_name, String speciality) {
+        this.doctor_id = doctor_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.speciality = speciality;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctors{" +
+                "doctor_id='" + doctor_id + '\'' +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", speciality='" + speciality + '\'' +
+                '}';
+    }
 }
