9cf254936288a1939282dc36a56344bd1c916350
DennyAntonova <83185824+DennyAntonova@users.noreply.github.com>
2021-11-06 00:52:32 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
 
+
+      public static Object read(String csvFile) {
+        List<String> usersList = new ArrayList<>();
+        try {
+            File file = new File(csvFile);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            usersList = bufferedReader.lines().collect(Collectors.toList());
+            bufferedReader.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return usersList;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        String csvFile = "D:\\DigitalMontana\\Hospital-master\\src\\com\\company\\doctors.csv";
+        System.out.println(read(csvFile).toString().replaceAll("[\\[\\],]", " "));
+
+
     }
 }
