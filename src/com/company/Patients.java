4ceeebdf24197a54d051e4753d31ee1a61ded073
stefanisenkova <83083926+stefanisenkova@users.noreply.github.com>
2021-11-06 14:07:32 +0200

    Update Patients.java

--- src/com/company/Patients.java
+++ src/com/company/Patients.java
@@ -1,17 +1,17 @@
 package com.company;
 
-public class Patients {
-   private static  int next_patient_id=1;
+public class Patient {
+    private static int next_patient_id = 1;
     private int patient_id;
     private String first_name;
     private String last_name;
     private int age;
-    //Constructor for Patient class-------------------------------------------------------------------------------------
+
     public Patient(int patient_id, String first_name, String last_name, int age) {
-        patient_id=next_patient_id++;
-        this.first_name=first_name;
-        this.last_name=last_name;
-        this.age=age;
+        patient_id = next_patient_id++;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.age = age;
     }
 
     public int getPatient_id() {
@@ -26,42 +26,34 @@ public class Patients {
         return last_name;
     }
 
+    public int getAge() {
+        return age;
+    }
+
     public void setPatient_id(int patient_id) {
         this.patient_id = patient_id;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
     }
 
     public void setFirst_name(String first_name) {
         this.first_name = first_name;
     }
 
-    public int getAge() {
-        return age;
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
     }
-    public static final String delimiter = ",";
-    public static void read(String csvFile) {
-        try {
-            File file = new File(csvFile);
-            FileReader fr = new FileReader(file);
-            BufferedReader br = new BufferedReader(fr);
-            String line = "";
-            String[] tempArr;
-            while((line = br.readLine()) != null) {
-                tempArr = line.split(delimiter);
-                for(String tempStr : tempArr) {
-                    System.out.print(tempStr + " ");
-                }
-                System.out.println();
-            }
-            br.close();
-        } catch(IOException ioe) {
-            ioe.printStackTrace();
-        }
-   }
+
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "doctor_id='" + patient_id + '\'' +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
