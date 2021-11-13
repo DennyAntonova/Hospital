37b1036e7802b0d564bf0c181b277f1410fd3607
DennyAntonova <83185824+DennyAntonova@users.noreply.github.com>
2021-11-13 22:59:27 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,34 +1,51 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Scanner;
+
+
+import static com.company.ReadFile.read;
 
 public class Main {
 
-
-      public static Object read(String csvFile) {
-        List<String> usersList = new ArrayList<>();
-        try {
-            File file = new File(csvFile);
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            usersList = bufferedReader.lines().collect(Collectors.toList());
-            bufferedReader.close();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to online system Hospital");
+        System.out.println("For doctors, press 1.");
+        System.out.println("For patients, press 2.");
+        int choice = Integer.parseInt(scanner.nextLine());
+        if (choice == 1) {
+            String doctors = "D:\\DigitalMontana\\Hospital-master\\src\\com\\company\\doctors.csv";
+            login((ArrayList<String>) read(doctors));
+        } else if (choice == 2) {
+            String patients = "D:\\DigitalMontana\\Hospital-master\\src\\com\\company\\patients.csv";
+            login((ArrayList<String>) read(patients));
         }
-        return usersList;
     }
 
-    public static void main(String[] args) {
-        String csvFile = "D:\\DigitalMontana\\Hospital-master\\src\\com\\company\\doctors.csv";
-        System.out.println(read(csvFile).toString().replaceAll("[\\[\\],]", " "));
+    public static void login(ArrayList<String> userList) {
 
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter user_ID");
+        String userId = scanner.nextLine();
+        System.out.println("Please enter your first name");
+        String name = scanner.nextLine();
+        System.out.println("Please enter your second name");
+        String secondName = scanner.nextLine();
+        boolean isContain = false;
+        for (String element : userList) {
+            if (element.contains(name) && (element.contains(secondName)) && (element.contains(userId))) {
+                System.out.printf("Welcome, %s %s!", name, secondName);
+                System.out.println();
+                isContain = true;
+                break;
+            }
+        }
+        if (!isContain) {
+            System.out.println("Wrong ID or Name. Please enter again!");
+            login(userList);
+        }
     }
 }
+
