2ee0fdbe776567d8566dc53e0f7661fb601bd817
DennyAntonova <83185824+DennyAntonova@users.noreply.github.com>
2021-11-13 23:06:00 +0200

    Update Doctor.java

--- src/com/company/Doctor.java
+++ src/com/company/Doctor.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class Doctors {
+public class Doctor {
     public String getDoctor_id() {
         return doctor_id;
     }
@@ -38,7 +38,7 @@ public class Doctors {
     private String last_name;
     private String speciality;
 
-    public Doctors(String doctor_id, String first_name, String last_name, String speciality) {
+    public Doctor(String doctor_id, String first_name, String last_name, String speciality) {
         this.doctor_id = doctor_id;
         this.first_name = first_name;
         this.last_name = last_name;
@@ -47,7 +47,7 @@ public class Doctors {
 
     @Override
     public String toString() {
-        return "Doctors{" +
+        return "Doctor{" +
                 "doctor_id='" + doctor_id + '\'' +
                 ", first_name='" + first_name + '\'' +
                 ", last_name='" + last_name + '\'' +
