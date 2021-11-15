6237fafc6c0fcadd8b3d3e7344361af2a33218a4
DennyAntonova <83185824+DennyAntonova@users.noreply.github.com>
2021-11-15 23:29:52 +0200

    Update User.java

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -1,4 +1,46 @@
 package com.company;
 
 public class User {
+    private int id;
+    private String firstName;
+    private String secondName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public User(int id, String firstName, String secondName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                '}';
+    }
 }
