87ae554515bc7255b800ea0a326b8cf12de3f7c8
stefanisenkova <83083926+stefanisenkova@users.noreply.github.com>
2021-11-16 09:16:21 +0200

    Update Patient.java

--- src/com/company/Patient.java
+++ src/com/company/Patient.java
@@ -1,41 +1,11 @@
 package com.company;
 
 public class Patient extends User{
- private int age;
+  private int age;
 
-    public Patient(int userID, String firstName, String secondName, int age) throws FileNotFoundException {
-        super(userID,firstName,secondName);
+    public Patient(int id, String firstName, String secondName, int age) throws FileNotFoundException {
+        super(id,firstName,secondName);
         this.age = age;
-    }
-
-    @Override
-    public int getUserID() {
-        return super.getUserID();
-    }
-
-    @Override
-    public void setUserID(int userID) {
-        super.setUserID(userID);
-    }
-
-    @Override
-    public String getFirstName() {
-        return super.getFirstName();
-    }
-
-    @Override
-    public void setFirstName(String firstName) {
-        super.setFirstName(firstName);
-    }
-
-    @Override
-    public String getSecondName() {
-        return super.getSecondName();
-    }
-
-    @Override
-    public void setSecondName(String secondName) {
-        super.setLastName(secondName);
     }
 
     public int getAge() {
@@ -46,10 +16,12 @@ public class Patient extends User{
         this.age = age;
     }
 
-
     @Override
     public String toString() {
         return "Patient{" +
+                "id=" + super.getId() +
+                ", firstName='" + super.getFirstName() + '\'' +
+                ", secondName='" + super.getSecondName() + '\'' +
                 "age=" + age +
                 '}';
     }
