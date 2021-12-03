c8b1690ebcc3dd0d73e7ff7c1f5395844918de33
Denitsa Antonova <dennyantonova@gmail.com>
2021-12-03 13:55:02 +0200

    fixup! diff

--- test/PatientTest.java
+++ test/PatientTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Objects;
 
 import static com.company.ReadFile.readAppointmentsFromCSV;
 
@@ -16,8 +17,8 @@ public class PatientTest {
     @Test
     public void testReversedHours() throws IOException {
         for (int i = 0; i < appointments.size(); i++) {
-            String expected = String.valueOf(appointments.get(i));
-            String actual = Patient.reversedHours(appointments.get(i), "1");
+            String expected = String.valueOf(appointments);
+            String actual = Patient.reversedHours(appointments, "1");
             String msg = "";
             Assert.assertEquals(expected, actual);
 
@@ -34,20 +35,20 @@ public class PatientTest {
 
     @Test
     public void testChangeDateMethod() throws IOException {
-
-        for (int i = 0; i < appointments.size(); i++) {
-            String expected = "Appointment{appointmentId=1, patient=Patient{age=25, id=1, firstName='Maria', secondName='Petrova'}, examination=INITIAL, date='15-08-2021', time='1500', doctor=Doctor{speciality=UROLOGY, id=1, firstName='Ivaylo', secondName='Petrov'}}";
-            String actual = Patient.cancelAppointment(appointments.get(0));
-            String msg = "";
-            Assert.assertEquals(expected, actual);
-        }
+        int appointmentId = 1;
+        String newDate = "25-06-1026";
+        String expected = "false";
+        String actual = String.valueOf(Objects.equals(appointments.get(appointmentId - 1).setDate(newDate), appointmentId));
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void testChangeTimeMethod(){
+    public void testChangeTimeMethod() {
 
     }
+
     @Test
-    public void testCancelAppointmentMethod(){
+    public void testCancelAppointmentMethod() throws IOException {
 
     }
 
