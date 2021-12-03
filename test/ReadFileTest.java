189d558137c12cbb131c8b1c90cd5886fcdae8a0
stefanisenkova <83083926+stefanisenkova@users.noreply.github.com>
2021-12-03 22:31:58 +0200

    tests

--- test/ReadFileTest.java
+++ test/ReadFileTest.java
@@ -1,22 +1,29 @@
-import com.company.Appointment;
-import com.company.Patient;
-import com.company.ReadFile;
-import com.company.SortingReversedHoursForDoctor;
+import com.company.*;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
-import static com.company.ReadFile.readAppointmentsFromCSV;
+import static com.company.ReadFile.*;
 
 public class ReadFileTest {
-  /*  Patient patient = new Patient(1, "Stefan", "Ivanov", 85);
+    ArrayList<User> patients = readPatientsFromCSV("patients");
+    ArrayList<User> doctors = readDoctorsFromCSV("doctors");
+
 
     @Test
-    public void testCreatePatient(){
-        Patient expected=  new Patient(1, "Stefan", "Ivanov", 85);
-        Patient actual= (Patient) ReadFile.createPatient(patient);
+    public void testFindPatientById() {
+        int idPatient = 1;
+        Patient expected = (Patient) patients.get(0);
+        Patient actual = ReadFile.findPatientById(patients, idPatient);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindDoctorById() {
+        int idDoctor=10;
+        Doctor expected = (Doctor) doctors.get(9);
+        Doctor actual = ReadFile.findDoctorById(doctors,idDoctor);
         Assert.assertEquals(expected,actual);
     }
-*/
 }
