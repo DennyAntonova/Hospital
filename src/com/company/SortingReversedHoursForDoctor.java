a84f69ebd29cfa912327c522a9fe1d909c5d0263
stefanisenkova <83083926+stefanisenkova@users.noreply.github.com>
2021-12-03 11:32:02 +0200

    tests

--- src/com/company/SortingReversedHoursForDoctor.java
+++ src/com/company/SortingReversedHoursForDoctor.java
@@ -4,38 +4,43 @@ package com.company;
 import java.util.ArrayList;
 
 public class SortingReversedHoursForDoctor {
-    public static void sortByPatientNameInAscendingOrder(ArrayList<Appointment> reversedHoursForDoctor) {
+    public static ArrayList<Appointment> sortByPatientNameInAscendingOrder(ArrayList<Appointment> reversedHoursForDoctor) {
         reversedHoursForDoctor.sort(Appointment::compareByNamePatient);
         System.out.println(reversedHoursForDoctor);
-
+        return reversedHoursForDoctor;
     }
-    public static void sortByPatientNameInDescendingOrder(ArrayList<Appointment> reversedHoursForDoctor) {
+
+    public static ArrayList<Appointment> sortByPatientNameInDescendingOrder(ArrayList<Appointment> reversedHoursForDoctor) {
         reversedHoursForDoctor.sort(Appointment::compareByNamePatientDescending);
         System.out.println(reversedHoursForDoctor);
-
+        return reversedHoursForDoctor;
 
     }
 
-    public static void SortingByAppointmentForExaminationOfPatientsInAscendingOrder
+    public static ArrayList<Appointment> SortingByAppointmentForExaminationOfPatientsInAscendingOrder
             (ArrayList<Appointment> reversedHoursForDoctor) {
         reversedHoursForDoctor.sort(Appointment::compareToDate);
         System.out.println(reversedHoursForDoctor);
+        return reversedHoursForDoctor;
     }
 
-    public static void SortingByAppointmentForExaminationOfPatientsInDescendingOrder
+    public static ArrayList<Appointment> SortingByAppointmentForExaminationOfPatientsInDescendingOrder
             (ArrayList<Appointment> reversedHoursForDoctor) {
         reversedHoursForDoctor.sort(Appointment::compareToDateDescending);
         System.out.println(reversedHoursForDoctor);
+        return reversedHoursForDoctor;
     }
 
-    public static void SortingByPatientsIdInAscendingOrder(ArrayList<Appointment> reversedHoursForDoctor) {
+    public static ArrayList<Appointment> SortingByPatientsIdInAscendingOrder(ArrayList<Appointment> reversedHoursForDoctor) {
         reversedHoursForDoctor.sort(Appointment::compareTo);
         System.out.println(reversedHoursForDoctor);
+        return reversedHoursForDoctor;
     }
-    public static void SortingByPatientsIdInDescendingOrder(ArrayList<Appointment> reversedHoursForDoctor) {
+
+    public static ArrayList<Appointment> SortingByPatientsIdInDescendingOrder(ArrayList<Appointment> reversedHoursForDoctor) {
         reversedHoursForDoctor.sort(Appointment::compareToPatientIdDescending);
         System.out.println(reversedHoursForDoctor);
-
+        return reversedHoursForDoctor;
     }
 }
 
